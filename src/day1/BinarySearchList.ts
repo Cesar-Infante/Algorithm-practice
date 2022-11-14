0786688e6208a764c2428af60455bdbc745d030c
Cesar Infante <104239687+Cesar-Infante@users.noreply.github.com>
2022-11-14 04:58:28 +0000

    Solving and understanding binary search algo

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,16 +1,34 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
     // lo is inclusive (+1) high is exclusive
     // search(arr, lo, hi) needle
-    // do { 
+    // do {
     // mid = lo + (hi - lo) / 2
     // value = arr[mid]
     // if v = needle
     // return true
-    // else if v > mid
+    // else if v > needle
+    // hi = mid
+    // else
     // lo = mid + 1
-    // else hi = mid
     // } while (lo < hi)
-    // return false || -1 
+    // return false || -1
+
     let lo = 0;
     let hi = haystack.length;
+
+    do {
+        // finds where the mid is at
+        let mid = Math.floor(lo + (hi - lo) / 2);
+        // keeps track of each new mid
+        let value = haystack[mid];
+
+        if (value === needle) {
+            return true;
+        } else if (value > needle) {
+            hi = mid; 
+        } else {
+            lo = mid + 1;
+        }
+    } while (lo < hi);
+    return false;
 }
