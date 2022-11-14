487f35d8373265f2fe32b503768c3d2b6e6f8f3f
Cesar Infante <104239687+Cesar-Infante@users.noreply.github.com>
2022-11-14 01:14:59 +0000

    Pseudo code of binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,4 +1,16 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    // lo is inclusive (+1) high is exclusive
+    // search(arr, lo, hi) needle
+    // do { 
+    // mid = lo + (hi - lo) / 2
+    // value = arr[mid]
+    // if v = needle
+    // return true
+    // else if v > mid
+    // lo = mid + 1
+    // else hi = mid
+    // } while (lo < hi)
+    // return false || -1 
     let lo = 0;
     let hi = haystack.length;
 }
